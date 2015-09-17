27bc7c6bee432424597f8b3f11d2a6cb34cc0fe3
Robert Northard <robertnorthard@googlemail.com>
2015-09-17 19:23:33 +0100

    Added create and delete post to blog service

--- app/scripts/services/blogService.js
+++ app/scripts/services/blogService.js
@@ -16,4 +16,12 @@ app.service("blogService", function($http){
 	this.findById = function (id){
 		return $http.get(properties.api + '/blog/posts/' + id);
 	};
+
+	this.createPost = function(){
+		return $http.post(properties.api + '/blog/posts');
+	};
+
+	this.deletePost = function(id){
+		return $http.delete(properties.api + '/blog/posts/' + id);
+	};
 });
