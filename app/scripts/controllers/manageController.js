f5d2a0e4c1fb9366234f41a8e32193ea30a6f63e
Robert Northard <robertnorthard@googlemail.com>
2015-09-18 13:22:28 +0100

    Added manage blog posts view

--- app/scripts/controllers/manageController.js
+++ app/scripts/controllers/manageController.js
@@ -1,5 +1,12 @@
 'use strict';
 
-app.controller('manageController', function($scope, $rootScope, AuthService){
+app.controller('manageController', function($scope, $rootScope, BlogService){
 
+	$scope.message = 'Manage Blog Posts';
+
+	BlogService.getPosts().success(function(data){
+		$scope.posts = data;
+	}).error(function(){
+		$scope.error = "Blog posts not found"
+	});
 });
