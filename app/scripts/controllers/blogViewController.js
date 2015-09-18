6d06bbb0f23fecab7801fa6c3c328a6f45d4b582
Robert Northard <robertnorthard@googlemail.com>
2015-09-18 20:10:38 +0100

    Bind content from API as html

--- app/scripts/controllers/blogViewController.js
+++ app/scripts/controllers/blogViewController.js
@@ -1,11 +1,13 @@
 'use strict';
 
-app.controller('blogViewController', function($routeParams, $scope, BlogService){
+app.controller('blogViewController', function($routeParams, $scope, $sce, BlogService){
 
 	$scope.message = 'Blog';
     	
 	BlogService.findById($routeParams.id).success(function(data){
 		$scope.post = data.data;
+		$scope.post.body = $sce.trustAsHtml(data.data.body);
+
 	}).error(function(){
 		$scope.error = "Blog post not found, " + $routeParams.id
 	});
